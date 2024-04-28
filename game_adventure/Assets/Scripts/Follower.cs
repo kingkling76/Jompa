ebc09b0dc19ba49c68126408d16e295e10336f13
axehuh201 <axehuh201@studentmail.oru.se>
2024-04-28 15:11:10 +0200

    lagt till förföljare

--- game_adventure/Assets/Scripts/Follower.cs
+++ game_adventure/Assets/Scripts/Follower.cs
@@ -4,9 +4,14 @@ using UnityEngine;
 
 public class Follower : MonoBehaviour
 {
-    public player player;
+    //public player player;
     public float followDistance = 2f; // Adjust this value as needed
 
+
+    private void Start()
+    {
+        transform.position = (Vector2)player.instance.transform.position + new Vector2(followDistance - 1, followDistance - 1);
+    }
     void Update()
     {
         Vector2 targetPosition = player.instance.transform.position;
@@ -17,9 +22,12 @@ public class Follower : MonoBehaviour
 
         if (distance > followDistance)
         {
-            transform.position = Vector2.MoveTowards(transform.position, targetPosition, player.instance.moveSpeed * Time.deltaTime);
+            if(distance > 10)
+                transform.position = Vector2.MoveTowards(transform.position, targetPosition, player.instance.moveSpeed * 100 * Time.deltaTime);
+            else
+                transform.position = Vector2.MoveTowards(transform.position, targetPosition, player.instance.moveSpeed * Time.deltaTime);
         }
-        else
+       else if (distance < 1.5)
         {
             // If the follower is too close, move away from the player
             Vector2 awayFromPlayer = (Vector2)transform.position - targetPosition;
